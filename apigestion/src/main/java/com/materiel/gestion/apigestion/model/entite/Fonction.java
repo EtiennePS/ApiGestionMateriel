f91099dda4b7a11aacdd7d4aca99a1ebb45474e4
PICHERIT <etienne.picherit@cgi.com>
2019-07-13 12:25:44 +0200

    ajout entite contact personne fonction

---
+++ apigestion/src/main/java/com/materiel/gestion/apigestion/model/entite/Fonction.java
@@ -0,0 +1,31 @@
+package com.materiel.gestion.apigestion.model.entite;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.Data;
+
+@Data @Entity
+@Table(name="fonction")
+public class Fonction {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id", length=11, nullable=false)
+	private Long id;
+	
+	@Column(name="libelle", length=10, nullable=false)
+	private String libelle;
+	
+	@JsonIgnore
+	@OneToMany(mappedBy="fonction")
+	private List<Contact> contacts;
+}
---
+++ apigestion/src/main/java/com/materiel/gestion/apigestion/model/entite/Personne.java
@@ -0,0 +1,41 @@
+package com.materiel.gestion.apigestion.model.entite;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.Data;
+
+@Data @Entity
+@Table(name="personne")
+public class Personne {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id", length=11, nullable=false)
+	private Long id;
+	
+	@Column(name="nom", length=50, nullable=false)
+	private String nom;
+	
+	@Column(name="prenom", length=50, nullable=false)
+	private String prenom;
+	
+	@Column(name="nom", length=14, nullable=true)
+	private String telephone;
+	
+	@Column(name="email", length=100, nullable=false)
+	private String email;
+	
+	@JsonIgnore
+	@OneToMany(mappedBy="personne")
+	private List<Contact> contacts;
+}
