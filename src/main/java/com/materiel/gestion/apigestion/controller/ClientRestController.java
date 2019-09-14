a91b861ac42a58c27e7d4a6cef39c295508c24ef
EtiennePS <etienne.picherit@gmail.com>
2019-09-14 13:20:09 +0200

    Ajout du format PDF pour l'extraction des incidents

--- src/main/java/com/materiel/gestion/apigestion/controller/ClientRestController.java
+++ src/main/java/com/materiel/gestion/apigestion/controller/ClientRestController.java
@@ -1,6 +1,12 @@
 package com.materiel.gestion.apigestion.controller;
 
-import com.materiel.gestion.apigestion.model.dto.Intervalle;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.PageSize;
+import com.itextpdf.text.html.simpleparser.HTMLWorker;
+import com.itextpdf.text.pdf.PdfWriter;
+import com.itextpdf.tool.xml.XMLWorkerHelper;
+import com.openhtmltopdf.pdfboxout.PdfRendererBuilder;
 import com.materiel.gestion.apigestion.model.entite.Client;
 import com.materiel.gestion.apigestion.model.entite.Contact;
 import com.materiel.gestion.apigestion.model.entite.Incident;
@@ -10,13 +16,35 @@ import com.materiel.gestion.apigestion.service.IContactService;
 import com.materiel.gestion.apigestion.service.IIncidentService;
 import com.materiel.gestion.apigestion.service.IMaterielService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.xhtmlrenderer.pdf.ITextRenderer;
 
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.StringReader;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.sql.Date;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -24,19 +52,19 @@ import java.util.List;
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class ClientRestController {
     @Autowired
-    IClientService service;
+    private IClientService service;
     
     @Autowired
-    IContactService contactService;
+    private IContactService contactService;
 
     @Autowired
-    IClientService clientService;
-
-    @Autowired
-    IMaterielService materielService;
+    private IMaterielService materielService;
     
     @Autowired
-    IIncidentService incidentService;
+    private IIncidentService incidentService;
+    
+    @Autowired
+    private ApplicationContext appContext;
 
     @GetMapping("/{id}")
     public Client getById(@PathVariable Long id) {
@@ -57,13 +85,13 @@ public class ClientRestController {
     @PutMapping("/{id}")
     public Client edit(@RequestBody Client client, @PathVariable Long id) {
         client.setId(id);
-        Client c = clientService.edit(client);
+        Client c = service.edit(client);
         return c;
     }
     @DeleteMapping("/{id}")
     public void deleteClient(@PathVariable Long id){
         Client c = new Client(id);
-        clientService.delete(c);
+        service.delete(c);
     }
 
     /****** CONTACT ******/
@@ -148,13 +176,65 @@ public class ClientRestController {
     
     /****** INCIDENTS ******/
     
-    @GetMapping(path = "/{id}/incidents")
-    public List<Incident> getIncidentsInIntervalle(@PathVariable Long id, @RequestParam Date dateDebut, @RequestParam Date dateFin) {
+    private List<Incident> getIncidentsInIntervalle(Long id, Date dateDebut, Date dateFin) {
     	dateDebut = dateDebut == null ? new Date(Long.MIN_VALUE) : dateDebut;
     	dateFin = dateFin == null ? new Date(Long.MAX_VALUE) : dateFin;
-    	
-    	// TODO vérifier dans les paramètres de la requête si le format voulu est json et pdf, et générer un PDF dans le deuxième cas
     	return incidentService.getByClientAndDates(id, dateDebut, dateFin);
     }
+    
+    @GetMapping(path = "/{id}/incidents")
+    public List<Incident> getIncidentsInIntervalleJson(@PathVariable Long id, @RequestParam Date dateDebut, @RequestParam Date dateFin) {
+    	return this.getIncidentsInIntervalle(id, dateDebut, dateFin);
+    }
+    
+    @GetMapping(path = "/{id}/incidents", produces = MediaType.TEXT_HTML_VALUE)
+    public String getIncidentsInIntervalleHtml(@PathVariable Long id, @RequestParam Date dateDebut, @RequestParam Date dateFin) {
+    	List<Incident> incidents = this.getIncidentsInIntervalle(id, dateDebut, dateFin);
+    	Client c = this.service.getById(id);
+    	return this.toHtml(incidents, c, dateDebut, dateFin);
+    }
+    
+    @GetMapping(path = "/{id}/incidents", produces = MediaType.APPLICATION_PDF_VALUE)
+    public ResponseEntity<byte[]> getIncidentsInIntervallePdf(@PathVariable Long id, @RequestParam Date dateDebut, @RequestParam Date dateFin) throws Exception {
+    	List<Incident> incidents = this.getIncidentsInIntervalle(id, dateDebut, dateFin);
+    	Client c = this.service.getById(id);
+    	String html = this.toHtml(incidents, c, dateDebut, dateFin);
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy.MM.dd");
+    	String filename = "incidents" + c.getNom() + "_" + sdf.format(dateDebut) + "_" + sdf.format(dateFin) + ".pdf";
+    	
+    	ByteArrayOutputStream pdfStream = new ByteArrayOutputStream();
+    	ITextRenderer renderer = new ITextRenderer();
+    	renderer.setDocumentFromString(html);
+    	renderer.layout();
+    	renderer.createPDF(pdfStream);
+    	pdfStream.close();
+    	
+    	HttpHeaders headers = new HttpHeaders();
+    	headers.setContentDispositionFormData(filename, filename);
+        headers.setCacheControl("must-revalidate, post-check=0, pre-check=0");
+        
+        return ResponseEntity.ok().headers(headers).body(pdfStream.toByteArray());
+    }
+    
+    private String toHtml(List<Incident> incidents, Client c, Date dateDebut, Date dateFin) {	
+		String template = "<!DOCTYPE html><html lang=\"fr\"><head><meta charset=\"utf-8\"/><title>Export incident</title><style>h1{text-align:center}table{border-collapse:collapse;width:90%;margin-left:5%;text-align:center}table td,table th{border:1px solid black}</style></head><body><h1>Incident actifs<br/>{CLIENT} du {DEBUT} à {FIN}</h1><table><tr><th>Id</th><th>Libellé</th><th>Date création</th><th>Date clôture</th><th>Symptôme</th><th>Résolution</th></tr>{LIGNES}</table></body></html>";
+		String td = "</td><td>";	
+		StringBuilder lignes = new StringBuilder();
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		
+		for(Incident i : incidents) {
+			lignes.append("<tr><td>").append(i.getId())
+					.append(td).append(i.getLibelle())
+					.append(td).append(i.getDateCreation() == null ? "-" : sdf.format(i.getDateCreation()))
+					.append(td).append(i.getDateCloture() == null ? "-" : sdf.format(i.getDateCloture()))
+					.append(td).append(i.getSymptome())
+					.append(td).append(i.getResolution()).append("</td></tr>");
+		}
+		
+		return template.replace("{LIGNES}", lignes.toString())
+				.replace("{DEBUT}", sdf.format(dateDebut))
+				.replace("{FIN}", sdf.format(dateFin))
+				.replace("{CLIENT}", c.getNom());
+	}
     
 }
--- src/main/java/com/materiel/gestion/apigestion/service/impl/IncidentService.java
+++ src/main/java/com/materiel/gestion/apigestion/service/impl/IncidentService.java
@@ -1,5 +1,6 @@
 package com.materiel.gestion.apigestion.service.impl;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
